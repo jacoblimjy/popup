55bad445a2c0d8596f25f12883e143e147f3bd7c
LohYangXian <e0726433@u.nus.edu>
2025-02-19 22:25:08 +0800

    removed test route for db connection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,13 +6,4 @@ router.get("/", (req, res) => {
   res.send("Welcome to the user route");
 });
 
-router.get("/test-db", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT 1 + 1 AS solution");
-    res.json({ solution: rows[0].solution });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 module.exports = router;
