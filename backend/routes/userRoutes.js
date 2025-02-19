821eab9651d194040fe0d7b4551ba9b4a280a5f4
LohYangXian <e0726433@u.nus.edu>
2025-02-19 22:02:44 +0800

    added dockercompose to set up sql, and added db connection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
 router.get("/", (req, res) => {
   res.send("Welcome to the user route");
+});
+
+router.get("/test-db", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT 1 + 1 AS solution");
+    res.json({ solution: rows[0].solution });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
