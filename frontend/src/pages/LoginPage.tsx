f6969cffb245023c4e0ed66591bc062b18d1e22a
giffordlee <giffordlee20@gmail.com>
2025-02-19 15:06:50 +0800

    feat(ui): Update login page ui

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -25,84 +25,113 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 items-center justify-center px-6 py-12 lg:px-8">
-        <div className="bg-gray-50 lg:w-[30%] w-full px-6 py-6 rounded-lg">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form onSubmit={handleSignIn} className="space-y-6">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm/6 font-medium text-gray-900"
-                  >
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a
-                      href="/forget-password"
-                      className="font-semibold text-blue-600 hover:text-blue-400"
+    <div className="flex h-full items-center py-16">
+      <div className="w-full max-w-md mx-auto p-6">
+        <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
+          <div className="p-4 sm:p-7">
+            <div className="text-center">
+              <h1 className="block text-2xl font-bold text-gray-800">Sign in</h1>
+
+            </div>
+            <div className="mt-5">
+              <form onSubmit={handleSignIn}>
+                <div className="grid gap-y-4">
+                  <div>
+                    <label htmlFor="email" className="block text-sm mb-2">
+                      Email address
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                        autoComplete="email"
+                        className="py-3 px-4 block w-full rounded-md bg-white text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+                      />
+                    </div>
+
+                  </div>
+                  <div>
+                    <div className="flex justify-between items-center">
+                      <label htmlFor="password" className="block text-sm mb-2">
+                        Password
+                      </label>
+                      <a
+                        className=" text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium mb-2"
+                        href="/forgot-password"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                    <div className="relative">
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                        className="py-3 px-4 block w-full rounded-md bg-white text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+
+                      />
+                      <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                        <svg
+                          className="size-5 text-red-500"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          aria-hidden="true"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p
+                      className="hidden text-xs text-red-600 mt-2"
+                      id="password-error"
                     >
-                      Forgot password?
-                    </a>
+                      8+ characters required
+                    </p>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="flex">
+                      <input
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                        className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500"
+                      />
+                    </div>
+                    <div className="ms-3">
+                      <label htmlFor="remember-me" className="text-sm">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  >
+                    Sign in
+                  </button>
+                  <div className="flex flex-col items-center">
+                    <p className="mt-2 text-sm text-gray-600">
+                      Don't have an account yet?{" "} 
+                      <a
+                        className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium"
+                        href="/signup"
+                      >
+                        Sign up
+                      </a>
+                    </p>
                   </div>
                 </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-            <p className="mt-8 text-center text-sm/6 text-gray-500">
-              Not a member yet?{" "}
-              <a
-                href="/signup"
-                className="font-semibold text-blue-600 hover:text-blue-400"
-              >
-                Sign Up
-              </a>
-            </p>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
