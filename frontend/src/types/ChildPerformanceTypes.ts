cb90bec12333ee62c30a2a034cfd6a4ce1f7528b
giffordlee <giffordlee20@gmail.com>
2025-04-15 19:05:34 +0800

    Update child_performance response type

--- frontend/src/types/ChildPerformanceTypes.ts
+++ frontend/src/types/ChildPerformanceTypes.ts
@@ -7,6 +7,15 @@ export interface ChildPerformance {
   average_time_per_question: number;
 }
 
+export interface ChildPerformanceResponse {
+  child_id: number;
+  topic_id: number;
+  total_time_spent: string;
+  total_questions_attempted: string;
+  average_accuracy_score: string;
+  average_time_per_question: string;
+}
+
 export interface OverallPerformance {
   total_questions_completed: number;
   overall_score: number;
